3c952bd0e3a363dcbd6e4ab49fe3a2f7ce95bfa8
0x7da <38230414+0x7da@users.noreply.github.com>
2022-04-02 15:57:44 +0200

    Add files via upload

---
+++ Quinn-Music/events/ready.js
@@ -0,0 +1,17 @@
+module.exports = (client) => {
+    console.log('Login as' + client.user.tag)
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+    console.log(`Quinn`);
+}
